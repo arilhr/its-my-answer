cb41849aaf90d578b0b5fc1e45982f4885cb1c58
arilhr <aurielharlih9213@gmail.com>
2022-04-07 12:30:12 +0700

    add probuilder package

--- Assets/Scripts/PlayerSpawner.cs
+++ Assets/Scripts/PlayerSpawner.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerSpawner : MonoBehaviour
+public class PlayerSpawner : MonoBehaviourPunCallbacks
 {
     public GameObject playerPrefab;
     public List<Transform> spawnPoints;
